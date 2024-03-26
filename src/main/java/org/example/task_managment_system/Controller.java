25d7e599b29dedf26ec6dfad66cc19801364cbf1
App11e <adamimnnn@gmail.com>
2024-03-27 03:04:01 +0800

    Added clock feature (NOT TESTD)

--- src/main/java/org/example/task_managment_system/Controller.java
+++ src/main/java/org/example/task_managment_system/Controller.java
@@ -1,11 +1,13 @@
 package org.example.task_managment_system;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -13,6 +15,8 @@ import javafx.event.ActionEvent;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -35,6 +39,11 @@ public class Controller implements Initializable {
     @FXML
     public Button btnTasks;
 
+    @FXML
+    private Label currentTime;
+    @FXML
+    private Label currentDate;
+
     //my bad - the freaking mouse event
     @FXML
     public void handleButtonClicks(ActionEvent mouseEvent) {
@@ -47,10 +56,30 @@ public class Controller implements Initializable {
         }
     }//Dashboard.fxml
 
+    public void timeNow() { //clock function (not tested yet)
+        Thread thread = new Thread(() -> {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MMMM dd, EEEE"); //format
+            try {
+                while (!Thread.currentThread().isInterrupted()) { //infinite loop
+                    Thread.sleep(1000);
+                    final String timeNow = timeFormat.format(new Date());
+                    final String dateNow = dateFormat.format(new Date());
+                    Platform.runLater(() -> {
+                       currentTime.setText(timeNow);
+                       currentDate.setText(dateNow); //labelname
+                    });
+                }
+            } catch (InterruptedException e) {
+                //do nothing
+            }
+        });
+        thread.start();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        timeNow();
     }
 
     public void loadStage(String fxml) {
